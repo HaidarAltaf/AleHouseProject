453fdb62444edbcfffcbf6890ea196f53bcfb887
Haidar <haidaraltaf@gmail.com>
2022-03-14 12:29:42 +0000

    added service and service methods

--- src/main/java/com/qa/demo/Service/AleHouseService.java
+++ src/main/java/com/qa/demo/Service/AleHouseService.java
@@ -1,42 +1,69 @@
-//package com.qa.demo.Service;
-//
-//import java.util.List;
-//
-//import org.springframework.stereotype.Service;
-//
-//import com.qa.demo.Entity.AleHouse;
-//
-//@Service
-//public class AleHouseService implements AleHouseServiceMethods<AleHouse>{
-//
-//	@Override
-//	public AleHouse create(AleHouse AleHouse) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public List<AleHouse> readAll() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public AleHouse readById(long id) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public AleHouse update(long id, AleHouse AleHouse) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public boolean delete(long id) {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
-//
-//}
+package com.qa.demo.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.demo.Entity.AleHouse;
+import com.qa.demo.Repo.AleHouseRepo;
+
+@Service
+public class AleHouseService implements AleHouseServiceMethods<AleHouse>{
+
+	private AleHouseRepo repo;
+
+	public AleHouseService(AleHouseRepo repo) {
+		this.repo = repo;
+	}
+
+	public AleHouseRepo getRepo() {
+		return repo;
+	}
+
+	public void setRepo(AleHouseRepo repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public AleHouse create(AleHouse AleHouse) {
+		return this.repo.save(AleHouse);
+	}
+
+	@Override
+	public List<AleHouse> readAll() {
+		return this.repo.findAll();
+	}
+
+	@Override
+	public AleHouse readById(long id) {
+		Optional<AleHouse> AleHouseId = this.repo.findById(id);
+		if(AleHouseId.isPresent()){
+			return AleHouseId.get();
+		}
+		return null;
+	}
+
+	@Override 
+	public AleHouse update(long id, AleHouse AleHouse) {
+		Optional<AleHouse> existing = this.repo.findById(id);
+		if(existing.isPresent()) {
+			AleHouse existance = existing.get();
+			existance.setId(id);
+			existance.setDrink(existance.getDrink());
+			existance.setFood(existance.getFood());
+			existance.setName(existance.getName());
+			existance.setPrice(existance.getPrice());
+			existance.setGameOfGwent(true);
+			return this.repo.saveAndFlush(existance);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+
+}
