7552ccce988afcc94f0b882fdc5af1a5b7fafdea
Haidar <haidaraltaf@gmail.com>
2022-03-14 12:22:00 +0000

    want to merge

---
+++ src/main/java/com/qa/demo/Service/AleHouseService.java
@@ -0,0 +1,42 @@
+//package com.qa.demo.Service;
+//
+//import java.util.List;
+//
+//import org.springframework.stereotype.Service;
+//
+//import com.qa.demo.Entity.AleHouse;
+//
+//@Service
+//public class AleHouseService implements AleHouseServiceMethods<AleHouse>{
+//
+//	@Override
+//	public AleHouse create(AleHouse AleHouse) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<AleHouse> readAll() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public AleHouse readById(long id) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public AleHouse update(long id, AleHouse AleHouse) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean delete(long id) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//}
